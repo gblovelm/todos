f2568d594076727ea24e1cf8a3871e4b35fb7844
Martin Lovell <gblovelm@gmail.com>
2019-10-18 10:52:38 +0100

    change from array of text to array of objects

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 var todoList = {
-  todos: ['item 1', 'item 2', 'item 3'],
-  displayTodos: function() {
-    console.log('My Todos', this.todos);
-  },
-  addTodo: function(todo) {
-    this.todos.push(todo);  // use the todos in this object as not available outside
-    this.displayTodos(); // use the method displayTodos
-  },
-  changeTodo: function(position, newValue) {
-    this.todos[position] = newValue;  // use the todos in this object
-    this.displayTodos();  // use the method displayTodos
-  },
-  deleteTodo: function(position) {
-    this.todos.splice(position, 1);  // use the todos in this object
-    this.displayTodos();  // use the method displayTodos
-  }
-};
+      todos: [],
+      displayTodos: function() {
+        console.log('My Todos', this.todos);
+      },
+      addTodo: function(todoText) {
+        this.todos.push({
+          todoText: todoText,
+          completed: false  // when add todo to list , wont be completed yet
+        });  // use the todos in this object as not available outside
+        this.displayTodos(); // use the method displayTodos
+      },
+      changeTodo: function(position, todoText) {
+       // this.todos[position] = newValue;  // use the todos in this object
+       this.todos[position].todoText = todoText; // just change todoText, not the whole object
+        this.displayTodos();  // use the method displayTodos
+      },
+      deleteTodo: function(position) {
+        this.todos.splice(position, 1);  // use the todos in this object
+        this.displayTodos();  // use the method displayTodos
+      },
+      toggleCompleted: function(position) {
+        var todo = this.todos[position]; // create new variable to keep code small
+        todo.completed = !todo.completed;  // flip false to true or vice versa
+        this.displayTodos();
+      }
+    };
